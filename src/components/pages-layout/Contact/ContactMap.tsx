88ea1295839e70b727df067477cdb780faab4aab
jihansoft <jihanuddinsoft@gmail.com>
2025-08-12 23:48:58 +0600

    location add

--- src/components/pages-layout/Contact/ContactMap.tsx
+++ src/components/pages-layout/Contact/ContactMap.tsx
@@ -31,6 +31,15 @@ export default function ContactMap() {
         mapEmbedUrl="https://maps.google.com/maps?width=600&height=400&hl=en&q=Global%20Business%20Center%2C%20Cornish%20Road%2C%20Doha%2C%20Qatar&t=&z=14&ie=UTF8&iwloc=B&output=embed" // Use the correct embed link
         mapLink="https://www.google.com/maps/place/Global+Business+Centre/@25.2792508,51.5492179,17z/data=!3m1!4b1!4m6!3m5!1s0x3e45c57a1e5989ad:0x735a51a8600ee447!8m2!3d25.279246!4d51.5517928!16s%2Fg%2F11c1qy6gws?entry=ttu&g_ep=EgoyMDI1MDQwMi4xIKXMDSoASAFQAw%3D%3D" // Optional
       />
+      <ContactMapCard
+        title={t("location4.title")}
+        address={t("location4.desc")}
+        // phone="+385 1 6064 420"
+        // email="info.hr@dekra.com"
+        hours="Workdays 08:30 – 16:30"
+        mapEmbedUrl="https://maps.app.goo.gl/qrYGpwUAGYvBnKzh8" // Use the correct embed link
+        mapLink="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d7297.253471147235!2d90.38766889552147!3d23.867384825741485!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sRoad%20Number%2012%2C%20Sector%20%3A%206%20%2C%20Uttara%20%2CDhaka%20-1230!5e0!3m2!1sen!2sbd!4v1755020716509!5m2!1sen!2sbd" // Optional
+      />
     </div>
   );
 }
