66eff3be2f7f9205dc099334134ebbef1cc87c10
jihansoft <jihanuddinsoft@gmail.com>
2025-08-12 19:09:01 +0600

    first commit

---
+++ src/i18n/type.ts
@@ -0,0 +1,29 @@
+export type AppLocale =
+  | "en"
+  | "bg"
+  | "bs"
+  | "sr"
+  | "hr"
+  | "ro"
+  | "sl"
+  | "sk"
+  | "el"
+  | "la";
+
+export type CountryCode = keyof typeof countryToLocale;
+
+export const countryToLocale = {
+  BG: "bg",
+  BA: "bs",
+  RS: "sr",
+  HR: "hr",
+  RO: "ro",
+  SI: "sl",
+  SK: "sk",
+  US: "en",
+  GB: "en",
+  DE: "en",
+  FR: "en",
+  EL: "el", // Greece
+  VA: "la", // Vatican City (Latin)
+} as const satisfies Record<string, AppLocale>;
