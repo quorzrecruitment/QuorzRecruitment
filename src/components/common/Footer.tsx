8af3f89a8b69b280a1188765a9afc0bf7fc2dbb0
jihansoft <jihanuddinsoft@gmail.com>
2025-08-13 11:55:27 +0600

    name hide old

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -26,6 +26,10 @@ const Footer = () => {
       country: t("location3.title"),
       address: t("location3.desc"),
     },
+    {
+      country: t("location4.title"),
+      address: t("location4.desc"),
+    },
   ];
 
   // const socialLinks = [
@@ -119,7 +123,7 @@ const Footer = () => {
             <h3 className="text-xl md:text-2xl lg:text-3xl font-bold text-black mb-4 md:mb-6 lg:mb-8">
               {t("title2")}
             </h3>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6 lg:gap-8">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 md:gap-6 lg:gap-2">
               {locations.map((location, index) => (
                 <div
                   key={index}
